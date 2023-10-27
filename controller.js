bd238149efd7bafd6f487cb138d995f3106d8e8e
Albertox64 <alberto.arias.x64@gmail.com>
2023-10-27 00:07:40 -0500

    minify update

--- controller.js
+++ controller.js
@@ -1,4 +1,10 @@
 const app = angular.module("firstModule", [])
-app.controller("firstController", ($scope) => {
+app.controller("firstController", ["$scope", ($scope) => {
     $scope.name = "Simio"
-})
+    $scope.quantity
+    $scope.commentaryArray
+
+    $scope.repeat = () => {
+        $scope.commentaryArray = new Array(Number($scope.quantity))
+    }
+}])
