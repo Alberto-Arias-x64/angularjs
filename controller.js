261668969916268ef6c2e289973d3d40b39e86c6
Alberto Arias <alberto.arias.x64@gmail.com>
2023-10-27 22:33:50 -0500

    controller

--- controller.js
+++ controller.js
@@ -1,10 +1,14 @@
 const app = angular.module("firstModule", [])
 app.controller("firstController", ["$scope", ($scope) => {
-    $scope.name = "Simio"
-    $scope.quantity
-    $scope.commentaryArray
+    $scope.name = "x64"
+    $scope.quantity = 0
+    $scope.commentaryArray = []
 
     $scope.repeat = () => {
-        $scope.commentaryArray = new Array(Number($scope.quantity))
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(res => {
+            $scope.commentaryArray = res
+        })
     }
 }])
