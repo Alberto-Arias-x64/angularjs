7358c14db6cfae578ca4d295a4d62cbbf58629d3
Alberto Arias <alberto.arias.x64@gmail.com>
2023-10-29 02:00:23 -0500

    ToDoService

--- controller.js
+++ controller.js
@@ -1,5 +1,6 @@
 const app = angular.module("firstModule", [])
-app.controller("firstController", ["$scope","$rootScope", ($scope,$rootScope) => {
+
+app.controller("firstController", ["$scope", "$rootScope","ToDoService", ($scope, $rootScope,ToDoService ) => {
 
     $scope.name = "x64"
     $scope.quantity = 0
@@ -29,16 +30,30 @@ app.controller("firstController", ["$scope","$rootScope", ($scope,$rootScope) =>
         $scope.task = {}
     }
 
+    $scope.delete = () => {
+        $scope.todoList = []
+        ToDoService.clean()
+    }
+
     // $scope.$watch(() => $scope.task,(element) => {
     //     console.log(element)
     // })
     $scope.$watchCollection('task', (element) => {
         console.log(element)
     })
+    /* mejor evitar watchers */
 }])
 
 app.filter('customFilter', () => {
     return (text) => {
         return text.toUpperCase()
     }
+})
+
+app.factory('ToDoService', () => {
+    const TODOService = {}
+    TODOService.clean = () => {
+        localStorage.clear()
+    }
+    return TODOService
 })
