0f518564640a9531a981900e039004c47ba9b746
Alberto Arias <alberto.arias.x64@gmail.com>
2023-10-28 23:19:54 -0500

    controller

--- controller.js
+++ controller.js
@@ -1,14 +1,29 @@
 const app = angular.module("firstModule", [])
 app.controller("firstController", ["$scope", ($scope) => {
+
     $scope.name = "x64"
     $scope.quantity = 0
     $scope.commentaryArray = []
+    $scope.task = {}
 
-    $scope.repeat = () => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(res => {
-            $scope.commentaryArray = res
+    if (localStorage.getItem('TODO')) $scope.todoList = JSON.parse(localStorage.getItem('TODO'))
+    else $scope.todoList = []
+
+    $scope.repeat = async () => {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const parsedRes = await res.json()
+        $scope.commentaryArray = parsedRes
+    }
+
+    $scope.addTask = () => {
+        $scope.todoList.push($scope.task)
+        const clearList = $scope.todoList.map(element => {
+            return {
+                description: element.description,
+                time: element.time
+            }
         })
+        localStorage.setItem('TODO', JSON.stringify(clearList))
+        $scope.task = {}
     }
 }])
